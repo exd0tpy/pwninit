dda92bae87077070c711f69fabb024df4f7b46eb
ICE_BEAR <jsw5258@naver.com>
2019-10-07 00:08:29 +0900

    Add -s option
    option --s, strip file extention

--- pwninit.py
+++ pwninit.py
@@ -25,13 +25,16 @@ C_BGCYAN   = "\033[46m"
 C_BGWHITE  = "\033[47m"
 
 info = "["+C_BOLD + C_GREEN+"+"+C_END+"]"
-success = "["+C_BOLD + C_GREEN+"Success!"+C_END+"]"
+info_success = "["+C_BOLD + C_GREEN+"Success!"+C_END+"]"
+info_failed = "["+C_BOLD + C_RED+"Failed!"+C_END+"]"
 
 parser = argparse.ArgumentParser(description='This tool is for pwnable binary')
 
-parser.add_argument('binary',type=str,metavar='binary_name',help='Enter binary path(name)')
+parser.add_argument('-s','--strip',help='strip file extention like .dms', action='store_true')
 
-parser.add_argument('--libc',type=str,default = '',help='Enter custom libc file(optional)')
+parser.add_argument('binary',type=str,metavar='binary_name',help='binary path(name)')
+
+parser.add_argument('--libc',type=str,default = '',help='custom libc file(optional)')
 
 
 args = parser.parse_args()
@@ -40,13 +43,31 @@ libc = args.libc
 
 print binary , libc
 
-def print_info(text):
-	space = '                            '
-	space = space[:-len(text)]
-	print info+text+space+success
+def print_info(text,flag):
+  space = '                            '
+  space = space[:-len(text)]
+  if flag:
+    print info+text+space+info_success
+  else:
+    print info+text+space+info_failed
+
+def success(text):
+  print_info(text,True)
+
+def failed(text):
+  print_info(text,False)
+
+ori_binary_name = sys.argv[1]
+if args.strip:
+  binary_name = ori_binary_name.split('.')[0]
+  
+  result=os.system('mv ./'+ori_binary_name+' ./'+binary_name)
+  if result == 0:
+    success('change name')
+  else:
+    failed('change name')
 
 
-binary_name = sys.argv[1]
 # = sys.argv[2]
 
 #exploit_data = "from pwn import*\ncontext.log_level='debug'\ns = process(\'"+binary_name+"\')\n\ns.interactive()"
@@ -55,14 +76,26 @@ context.log_level='debug'
 s=process([\''''+binary_name+'''\'],env={"LD_PRELOAD":"'''+libc+'''"})
 s.interactive()'''
 
-os.system('chmod +x '+binary_name)
-print_info('chmod')
-os.system('checksec '+binary_name)
-print_info('checksec')
 
-f = open(binary_name+"_ex"+".py","wb")
+result = os.system('chmod +x '+binary_name)
+if result == 0:
+  success('chmod')
+else:
+  failed('chmod')
 
-f.write(exploit_data)
-print_info('exploit file create')
+
+result = os.system('checksec '+binary_name)
+if result == 0:
+  success('checksec')
+else:
+  failed('checksec')
+
+
+try:
+  f = open(binary_name+"_ex"+".py","wb")
+  f.write(exploit_data)
+  success('exploit file create')
+except:
+  failed('exploit file create')
 
 
